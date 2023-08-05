1ef11187754fbda87ca9bc6f7e51a5c19ce85afc
Maria Rykova <maria.nigay@yandex.ru>
2023-08-05 18:51:08 +0400

    add site rent car landing

---
+++ gallery.js
@@ -0,0 +1,105 @@
+const gallery = document.querySelector(".car_cards_gallery");
+let fetchedArray;
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    fetchedArray = data;
+    showCarCardsGallery(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching carousel data:", error);
+  });
+
+function showCarCardsGallery(data) {
+  let cardsNew = "";
+  data.map((card) => {
+    const images = card.images.map((image) => {
+      return `<img class="car_image_card car_image_card_gallery " src=${image.img} alt="" />`;
+    });
+    const newImages = images.join(" ");
+    cardsNew += `
+        <div class="card_container" >
+
+        <div class="slider-container card_container_gallery">
+            <div class="slider slider_gallery">
+                 <div class="slides">${newImages}</div>
+            </div>
+            <button class="prev-btn">Previous</button>
+            <button class="next-btn">Next</button>
+        </div>
+
+        <div class="card_car_info" data-card-id=${card.id}>
+                      <div class="card_car_title">${card.title}</div>
+                      <div class="card_car_year"><strong>Year:</strong> ${card.year}</div>
+                      <div class="card_car_model"><strong>Model:</strong> ${card.model}</div>
+                      <div class="card_car_make"><strong>Make:</strong> ${card.make}</div>
+                      <div class="card_car_description"><strong>Desription:</strong> ${card.description}</div>
+                    </div>
+                  </div>
+        `;
+  });
+  gallery.innerHTML = cardsNew;
+}
+
+// SLIDE FUNCTION
+
+// Function to create a slider for each card
+function createSlider(card) {
+  const slider = card.querySelector(".slider");
+  const prevBtn = card.querySelector(".prev-btn");
+  const nextBtn = card.querySelector(".next-btn");
+  const slides = card.querySelector(".slides");
+
+  console.log(card);
+
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    const slideWidth = slider.clientWidth;
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+
+  prevBtn.addEventListener("click", () => {
+    console.log("prev");
+    currentIndex =
+      (currentIndex - 1 + slides.children.length) % slides.children.length;
+    showSlide(currentIndex);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    console.log("next");
+
+    currentIndex = (currentIndex + 1) % slides.children.length;
+    showSlide(currentIndex);
+  });
+
+  // Show the initial slide
+  showSlide(currentIndex);
+}
+
+// Function to observe changes in the card gallery and initialize sliders for new cards
+function observeCardGallery() {
+  const cardContainer = document.querySelector(".car_cards_gallery");
+  // Create a MutationObserver to observe changes in the card gallery
+  const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        mutation.addedNodes.forEach((node) => {
+          if (node.classList && node.classList.contains("card_container")) {
+            const cards = document.querySelectorAll(".card_container");
+            cards.forEach((card) => {
+              createSlider(card);
+            });
+          }
+        });
+      }
+    });
+  });
+
+  // Start observing the card gallery for changes
+  observer.observe(cardContainer, { childList: true });
+}
+
+// Call the function to observe the card gallery for changes
+observeCardGallery();
---
+++ script.js
@@ -0,0 +1,131 @@
+const carousel = document.querySelector(".carousel");
+const prevBtn = document.querySelector(".arrow-right");
+const nextBtn = document.querySelector(".arrow-left");
+const prevBtnMobile = document.querySelector(".arrow-left-mobile");
+const nextBtnMobile = document.querySelector(".arrow-right-mobile");
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    showCarCardsCarousel(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching carousel data:", error);
+  });
+
+const slideStep = 1;
+const slideDistance = 345;
+let currentPosition = 0;
+
+function moveCarousel(direction) {
+  const carouselWidth = carousel.offsetWidth;
+  const maxPosition = carousel.scrollWidth - carouselWidth;
+  const newPosition =
+    direction === "left"
+      ? currentPosition - slideStep * slideDistance
+      : currentPosition + slideStep * slideDistance;
+
+  console.log("click1");
+  if (newPosition <= 0 && newPosition >= -maxPosition) {
+    carousel.style.transform = `translateX(${newPosition}px)`;
+    currentPosition = newPosition;
+  }
+}
+
+prevBtn.addEventListener("click", () => moveCarousel("right"));
+nextBtn.addEventListener("click", () => moveCarousel("left"));
+prevBtnMobile.addEventListener("click", () => moveCarousel("left"));
+nextBtnMobile.addEventListener("click", () => moveCarousel("right"));
+
+function showCarCardsCarousel(data) {
+  let carouselNew = "";
+
+  for (let i = 0; i < 6; i++) {
+    const images = data[i].images.map((image) => {
+      return `<img class="car_image_card" src=${image.img} alt="" />`;
+    });
+    const newImages = images.join(" ");
+    carouselNew += `<div class="card_container">
+    <div class="slider-container">
+    <div class="slider">
+         <div class="slides">${newImages}</div>
+    </div>
+    <button class="prev-btn">Previous</button>
+    <button class="next-btn">Next</button>
+</div> 
+                <div class="card_car_info">
+                  <div class="card_car_title">${data[i].title}</div>
+                  <div class="card_car_year"><strong>Year:</strong> ${data[i].year}</div>
+                  <div class="card_car_model"><strong>Model:</strong> ${data[i].model}</div>
+                  <div class="card_car_make"><strong>Make:</strong> ${data[i].make}</div>
+                  <div class="card_car_description"><strong>Desription:</strong> ${data[i].description}</div>
+                </div>
+              </div>
+    `;
+  }
+  carousel.innerHTML = carouselNew;
+}
+
+// SLIDE FUNCTION
+
+function createSlider(card) {
+  const slider = card.querySelector(".slider");
+  const prevBtn = card.querySelector(".prev-btn");
+  const nextBtn = card.querySelector(".next-btn");
+  const slides = card.querySelector(".slides");
+
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    const slideWidth = slider.clientWidth;
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+
+  prevBtn.addEventListener("click", () => {
+    currentIndex =
+      (currentIndex - 1 + slides.children.length) % slides.children.length;
+    showSlide(currentIndex);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % slides.children.length;
+    showSlide(currentIndex);
+  });
+
+  showSlide(currentIndex);
+}
+
+function observeCardGallery() {
+  const cardContainer = document.querySelector(".carousel");
+
+  const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        mutation.addedNodes.forEach((node) => {
+          if (node.classList && node.classList.contains("card_container")) {
+            const cards = document.querySelectorAll(".card_container");
+            cards.forEach((card) => {
+              createSlider(card);
+            });
+          }
+        });
+      }
+    });
+  });
+
+  observer.observe(cardContainer, { childList: true });
+}
+
+observeCardGallery();
+
+// BURGER FUNCTION
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    menu.classList.toggle("show");
+  });
+});
