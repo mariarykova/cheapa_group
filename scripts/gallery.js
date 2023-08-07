4837a6d344ae4bc3c0d8bf67ae576a017df47e6e
Maria Rykova <maria.nigay@yandex.ru>
2023-08-07 12:30:31 +0400

    make some comment changes

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,7 +1,7 @@
 const gallery = document.querySelector(".car_cards_gallery");
 let fetchedArray;
 
-fetch("./data.json")
+fetch("../data.json")
   .then((response) => response.json())
   .then((data) => {
     fetchedArray = data;
